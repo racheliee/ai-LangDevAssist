09d70267563da5af92655925c1157b91876de81e
mnseok <min.seok@g.skku.edu>
2024-07-28 12:51:03 +0900

    feat(be): docker init

--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -3,9 +3,10 @@ import { UsersService } from './users.service';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { UsersController } from './users.controller';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [ConfigModule, PrismaModule],
+  imports: [ConfigModule, PrismaModule, JwtModule],
   providers: [UsersService],
   exports: [UsersService],
   controllers: [UsersController],
