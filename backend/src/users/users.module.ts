83ae65f7e91a18d26973e0ef8f6facde11c1d3fb
rachelie <rachelie.dwg@gmail.com>
2024-07-28 18:12:47 +0900

    fix(users): resolved DI issue by adding forwardRef in UsersModule

--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -4,9 +4,10 @@ import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { UsersController } from './users.controller';
 import { JwtModule } from '@nestjs/jwt';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports: [ConfigModule, PrismaModule, JwtModule],
+  imports: [ConfigModule, PrismaModule, JwtModule, forwardRef(() => AuthModule)],
   providers: [UsersService],
   exports: [UsersService],
   controllers: [UsersController],
