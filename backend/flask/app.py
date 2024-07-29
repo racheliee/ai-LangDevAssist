3eda031114d68b54597680c2de22176b111da8b2
mnseok <min.seok@g.skku.edu>
2024-07-29 10:53:47 +0900

    feat(be): implement endpoint

--- backend/flask/app.py
+++ backend/flask/app.py
@@ -1,10 +1,22 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return 'Hello Flask World'
+
+@app.route('/generate_problem', methods=['POST'])
+def generate_problem():
+    data = request.get_json()
+    
+    return 'Problem Generated'
+
+@app.route('/generate_feedback', methods=['POST'])
+def generate_feedback():
+    data = request.get_json()
+    
+    return 'Feedback Generated'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
