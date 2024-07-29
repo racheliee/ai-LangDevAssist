1ec07394aa48779e254648404c96c3ea46c80f5e
mnseok <min.seok@g.skku.edu>
2024-07-30 07:53:05 +0900

    feat(be): log every network

--- backend/main/src/app.module.ts
+++ backend/main/src/app.module.ts
@@ -1,10 +1,15 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ChatModule } from './chat/chat.module';
+import { LoggerMiddleware } from 'common/middlewares/logger.middleware';
 
 @Module({
   imports: [AuthModule, UsersModule, PrismaModule, ChatModule],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
