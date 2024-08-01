b2386a63fce40e95be0b0cb42e08ef7dc725e1bf
mnseok <min.seok@g.skku.edu>
2024-08-01 22:02:32 +0900

    debug(be): debugging problem id

--- backend/main/src/chat/chat.controller.ts
+++ backend/main/src/chat/chat.controller.ts
@@ -45,7 +45,10 @@ export class ChatController {
   ) {
     const { problemId } = body;
     const { user } = req;
-    this.logger.log(`Generating feedback for problemId: ${problemId}`);
+
+    this.logger.log(
+      `body: ${body}\n Generating feedback for problemId: ${problemId}`,
+    );
     try {
       const feedback = await this.chatService.generateFeedback(
         problemId,
